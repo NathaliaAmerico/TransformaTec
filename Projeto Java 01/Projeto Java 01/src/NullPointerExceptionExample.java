de16456bbd95f279250ac8c261a4eee31f330306
Nathália Américo <Namericonascimento@gmail.com>
2022-07-10 16:53:26 -0300

    Primeiro Commit

---
+++ Projeto Java 01/Projeto Java 01/src/NullPointerExceptionExample.java
@@ -0,0 +1,17 @@
+public class NullPointerExceptionExample {
+    
+}
+package exceptions;
+
+public class NullPointerExceptionExample {
+    
+    public static void main(String[] args) {
+        
+        Integer number = null;
+
+        if (number > 0) {
+            System.out.println("Positivo");
+        }
+    }
+
+}
